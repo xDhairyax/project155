3bd66e5434d112cfcef61a7b44f1b93902ebc3ae
xDhairyax <66120130+xDhairyax@users.noreply.github.com>
2022-12-11 16:51:06 +0530

    Add files via upload

---
+++ components/Coins.js
@@ -0,0 +1,39 @@
+AFRAME.registerComponent("coins", {
+    init: function () {
+      for (var i = 1; i <= 20; i++) {
+        const id = `coin${i}`;
+   
+        const posX =Math.random() * 100 + -50      
+        const posY = Math.random() * 3+50
+        const posZ = Math.random() * 120
+  
+        const position = { x: posX, y: posY, z: posZ };
+
+        this.createCoins(id, position);
+      }
+    },
+  
+    createCoins: function(id, position) { 
+      
+      var treasureEl = document.querySelector("#island");
+  
+      var coinEl = document.createElement("a-entity");
+  
+      coinEl.setAttribute("id",id);
+      coinEl.setAttribute("position",position);
+      coinEl.setAttribute("scale", { x: 0.3, y: 0.3, z: 0.3 });
+      
+      coinEl.setAttribute("gltf-model", "./assets/coins/scene.gltf")
+      
+      coinEl.setAttribute("geometry",{ primitive: "circle",radius: 1 });  
+      
+      coinEl.setAttribute("animation",{
+          property:"rotation",
+          to:"0 360 0",
+          loop:"true",
+          dur:1000,
+      })
+  
+      treasureEl.appendChild(coinEl);
+    }
+  });
---
+++ components/Rotation.js
@@ -0,0 +1,88 @@
+AFRAME.registerComponent("island-rotation",{
+    schema:{
+        speedofrotation:{type:"number",default:0}
+    },
+    
+    init:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(e.key==="ArrowRight"){
+                if(this.data.speedofrotation<0.1){
+                    this.data.speedofrotation+=0.1
+                }
+            }
+
+            if(e.key==="ArrowLeft"){
+                if(this.data.speedofrotation>0.1){
+                    this.data.speedofrotation-=0.1
+                }
+            }
+        })
+    },
+
+    tick:function(){
+        mapRotation=this.el.getAttribute("rotation")
+        mapRotation.y+=this.data.speedofrotation
+        this.el.setAttribute("rotation",{
+            x:mapRotation.x,
+            y:mapRotation.y,
+            z:mapRotation.z,
+        })
+    }
+    
+    })
+    
+    AFRAME.registerComponent("diver-rotation", {
+        schema: {
+          speedOfRotation: { type: "number", default: 0 },
+          speedOfAscent: { type: "number", default: 0 }
+        },
+
+        init:function(){
+            window.addEventListener("keydown",(e)=>{
+    
+            this.data.speedOfRotation = this.el.getAttribute("rotation")     
+            this.data.speedOfAscent = this.el.getAttribute("position")
+    
+            var diverRotation = this.data.speedOfRotation
+            var diverPosition = this.data.speedOfAscent
+    
+            if(e.key==="ArrowRight"){
+                if(diverRotation.x<10){
+                    diverRotation.x+=1
+                    this.el.setAttribute("rotation",diverRotation)
+                }
+            }
+    
+            if(e.key==="ArrowLeft"){
+                if(diverRotation.x>-10){
+                    diverRotation.x-=1
+                    this.el.setAttribute("rotation",diverRotation)
+                }
+            }
+    
+            if(e.key==="ArrowUp"){
+                if(diverRotation.z<20){
+                    diverRotation.z+=0.5
+                    this.el.setAttribute("rotation",diverRotation)
+                }
+                if(diverPosition.y<2){
+                    diverPosition.y+=1
+                    this.el.setAttribute("position",diverPosition)
+                }
+            }
+    
+            if(e.key==="ArrowDown"){
+                if(diverRotation.z>-10){
+                    diverRotation.z-=0.5
+                    this.el.setAttribute("rotation",diverRotation)
+                }
+                if(diverPosition.y>-2){
+                    diverPosition.y-=1
+                    this.el.setAttribute("position",diverPosition)
+                }
+            }
+    
+        })
+        }
+    
+    })
